809fa504be0c9d5f58c866935b2664e1e4ea0da6
David Obando <daobando@microsoft.com>
2016-03-26 10:46:00 -0700

    Fix to CLR servergc setting

--- src/9.basickestrel-clr/run.cmd
+++ src/9.basickestrel-clr/run.cmd
@@ -1,4 +1,4 @@
 dotnet restore
 dotnet build --configuration release
-copy /y .\8.basickestrel-clr.runtimeconfig.json .\bin\release\net451\win7-x64\9.basickestrel-clr.runtimeconfig.json
+copy /y .\9.basickestrel-clr.config .\bin\release\net451\win7-x64\9.basickestrel-clr.config
 .\bin\release\net451\win7-x64\9.basickestrel-clr.exe
