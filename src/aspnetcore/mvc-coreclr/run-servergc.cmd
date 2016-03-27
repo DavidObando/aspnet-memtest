215b857aa11feaa96ecdecf6e5e1ff4ee6fe4475
David Obando <daobando@microsoft.com>
2016-03-26 19:58:33 -0700

    [TEMP] Setting COREHOST_SERVER_GC=1 for servergc scenarios

--- src/aspnetcore/mvc-coreclr/run-servergc.cmd
+++ src/aspnetcore/mvc-coreclr/run-servergc.cmd
@@ -1,3 +1,4 @@
+set COREHOST_SERVER_GC=1
 dotnet restore
 dotnet build --configuration release
 copy /y .\servergc.json .\bin\release\netstandardapp1.5\win7-x64\mvc-coreclr.runtimeconfig.json
--- src/aspnetcore/mvc-coreclr/run-workstationgc.cmd
+++ src/aspnetcore/mvc-coreclr/run-workstationgc.cmd
@@ -1,3 +1,4 @@
+set COREHOST_SERVER_GC=0
 dotnet restore
 dotnet build --configuration release
 copy /y .\workstationgc.json .\bin\release\netstandardapp1.5\win7-x64\mvc-coreclr.runtimeconfig.json
